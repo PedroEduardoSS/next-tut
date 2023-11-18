d3641cc49a32f219609b5080c5347f2d3c9a1ab7
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-11-18 17:44:22 -0300

    Test: testing jest

--- nexttut/jest.config.mjs
+++ nexttut/jest.config.mjs
@@ -1,5 +1,5 @@
 import nextJest from 'next/jest.js'
- 
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
@@ -10,7 +10,7 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
- 
+  verbose: true,
   testEnvironment: 'jest-environment-jsdom',
 }
  
